018229d59d1e16283705ed0d6bb2f8f90f4ae772
dany <danyeal.mateen@hotmail.com>
2023-01-11 22:59:05 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [equippedItem, setEquippedItem] = useState();
+  const [attackHuman, setAttackHuman] = useState();
+
+  const sword = {
+    dmg: 10,
+  };
+
+  const human = {
+    health: 10,
+  };
+
+  function clickedEq() {
+    setEquippedItem(sword);
+    console.log("Goblino has equipped the Sword +10 dmg");
+  }
+
+  function attackEnemy() {
+    human.health = human.health - sword.dmg;
+    console.log(
+      `You have attacked Human. Human has ${human.health} left. Human dies.`
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="content-container">
+        <h1>Goblino City</h1>
+        <button onClick={clickedEq}>Equip Sword</button>
+        <button onClick={attackEnemy}>Attack</button>
+      </div>
     </div>
   );
 }
